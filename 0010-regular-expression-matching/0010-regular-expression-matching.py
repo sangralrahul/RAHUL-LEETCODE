105771f1a0e990b5d0aa722a8ab30a03adfc5dda
RAHUL SANGRAL <rahul.rishusangral@gmail.com>
2026-08-13 00:25:38 +0530

    Time: 5 ms (65.02%), Space: 19.4 MB (50.37%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,17 +1,22 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        m, n = len(s), len(p)
+        rows, cols = len(s), len(p)
+        dp = [[False] * (cols + 1) for _ in range(rows + 1)]
+        dp[rows][cols] = True
 
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
-        dp[m][n] = True
+        for i in range(rows, -1, -1):
+            for j in range(cols - 1, -1, -1):
+                first_match = (
+                    i < rows and (s[i] == p[j] or p[j] == ".")
+                )
 
-        for i in range(m, -1, -1):
-            for j in range(n - 1, -1, -1):
-                first_match = i < m and (p[j] == s[i] or p[j] == ".")
-
-                if j + 1 < n and p[j + 1] == "*":
-                    dp[i][j] = dp[i][j + 2] or (first_match and dp[i + 1][j])
+                if j + 1 < cols and p[j + 1] == "*":
+                    dp[i][j] = dp[i][j + 2] or (
+                        first_match and dp[i + 1][j]
+                    )
                 else:
-                    dp[i][j] = first_match and dp[i + 1][j + 1]
+                    dp[i][j] = (
+                        first_match and dp[i + 1][j + 1]
+                    )
 
         return dp[0][0]
